d1a0cea20bd6f2e9a3a5b664b73ca4ee8db6cf2f
Vyoker <mochamadalfi21@gmail.com>
2025-11-20 11:46:36 +0700

    Create drop.py

---
+++ combat/drop.py
@@ -0,0 +1,23 @@
+import random, time
+from core.utils import slow
+from core.data_store import DROP
+
+def drop_item(player, monster_name="default"):
+    data = DROP
+    # Kalau monster punya loot khusus
+    loot = data.get("monsters", {}).get(monster_name, data.get("default", {}))
+    chance = loot.get("chance", 0)
+    # Tidak dapat apa-apa
+    if random.randint(1, 100) > chance:
+        return
+    items = loot.get("items", {})
+    if not items:
+        return
+    # Pilih item random
+    item = random.choice(list(items.keys()))
+    min_q, max_q = items[item]
+    qty = random.randint(min_q, max_q)
+    # Tambahkan ke inventory
+    player["inventory"][item] = player["inventory"].get(item, 0) + qty
+    slow(f"🎁 Zombie menjatuhkan {qty}x {item}!", 0.02)
+    time.sleep(0.6)
